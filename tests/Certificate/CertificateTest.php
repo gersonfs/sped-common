c998c38fe4552df5544efc2e89c515be271d3349
Gerson Felipe Schwinn <gerson@onehost.com.br>
2026-05-07 13:39:28 -0300

    Corrige timezone de validFrom/validTo do certificado
    openssl_x509_parse retorna as datas em UTC (sufixo Z). Sem o
    DateTimeZone explícito, createFromFormat usa a tz default do PHP e
    deturpa o valor absoluto, fazendo com que datas fiquem 3 h adiantadas
    em ambientes com tz America/Sao_Paulo.

--- tests/Certificate/CertificateTest.php
+++ tests/Certificate/CertificateTest.php
@@ -18,8 +18,9 @@ class CertificateTest extends \PHPUnit\Framework\TestCase
     {
         $certificate = Certificate::readPfx(file_get_contents(__DIR__ . self::TEST_PFX_FILE), 'associacao');
         $this->assertEquals('NFe - Associacao NF-e:99999090910270', $certificate->getCompanyName());
-        $this->assertEquals(new \DateTime('2009-05-22 17:07:03'), $certificate->getValidFrom());
-        $this->assertEquals(new \DateTime('2010-10-02 17:07:03'), $certificate->getValidTo());
+        $utc = new \DateTimeZone('UTC');
+        $this->assertEquals(new \DateTime('2009-05-22 17:07:03', $utc), $certificate->getValidFrom());
+        $this->assertEquals(new \DateTime('2010-10-02 17:07:03', $utc), $certificate->getValidTo());
         $this->assertTrue($certificate->isExpired());
         $dataSigned = $certificate->sign('nfe');
         $this->assertTrue($certificate->verify('nfe', $dataSigned));
@@ -34,8 +35,9 @@ class CertificateTest extends \PHPUnit\Framework\TestCase
         );
         $this->assertInstanceOf(Certificate::class, $certificate);
         $this->assertEquals('NFe - Associacao NF-e:99999090910270', $certificate->getCompanyName());
-        $this->assertEquals(new \DateTime('2009-05-22 17:07:03'), $certificate->getValidFrom());
-        $this->assertEquals(new \DateTime('2010-10-02 17:07:03'), $certificate->getValidTo());
+        $utc = new \DateTimeZone('UTC');
+        $this->assertEquals(new \DateTime('2009-05-22 17:07:03', $utc), $certificate->getValidFrom());
+        $this->assertEquals(new \DateTime('2010-10-02 17:07:03', $utc), $certificate->getValidTo());
         $this->assertTrue($certificate->isExpired());
         $dataSigned = $certificate->sign('nfe');
         $this->assertTrue($certificate->verify('nfe', $dataSigned));
