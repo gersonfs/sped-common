3d3b6aaa2df464159d9737cb0da18b03a405b918
Gerson Felipe Schwinn <gerson@onehost.com.br>
2026-09-25 11:11:07 -0300

    Chama curl_close apenas em PHP < 8.0
    A partir do PHP 8.0 curl_close não tem efeito e no PHP 8.5 foi depreciado.

--- src/Soap/SoapCurl.php
+++ src/Soap/SoapCurl.php
@@ -119,7 +119,9 @@ class SoapCurl extends SoapBase implements SoapInterface
             }
             $headsize = curl_getinfo($oCurl, CURLINFO_HEADER_SIZE);
             $httpcode = curl_getinfo($oCurl, CURLINFO_HTTP_CODE);
-            curl_close($oCurl);
+            if (PHP_VERSION_ID < 80000) {
+                curl_close($oCurl);
+            }
             $this->responseHead = trim(substr($response, 0, $headsize));
             $this->responseBody = trim(substr($response, $headsize));
             $this->saveDebugFiles(
@@ -224,7 +226,9 @@ class SoapCurl extends SoapBase implements SoapInterface
             }
             $headsize = curl_getinfo($oCurl, CURLINFO_HEADER_SIZE);
             $httpcode = curl_getinfo($oCurl, CURLINFO_HTTP_CODE);
-            curl_close($oCurl);
+            if (PHP_VERSION_ID < 80000) {
+                curl_close($oCurl);
+            }
             $this->responseHead = trim(substr($response, 0, $headsize));
             $this->responseBody = trim(substr($response, $headsize));
         } catch (\Exception $e) {
